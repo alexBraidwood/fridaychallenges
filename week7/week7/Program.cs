c5aaa294ef552ed1cd5d340543b250782e12938d
Alex <maddogmcclure@gmail.com>
2015-01-09 11:08:39 -0500

    Added Week 7, implement a set

---
+++ week7/week7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Set s1 = new Set(new int[] { 1, 2, 1, 15, 45, 11, 14 });
+            Set s2 = new Set(new List<int>() { 2, 45, 15, 26, 84, 23, 55, 98, 105, 32 });
+            Set s3 = new Set(new int[] { 2, 1, 45, 15, 14, 11 });
+
+            Set s1Copy = new Set(s1.Items);
+            Set s1s2Union = Set.Union(s1, s2);
+            Set s1s2Intersection = Set.Intersection(s1, s2);
+
+            Console.WriteLine("\n\n\n-----Objects------");
+            Console.WriteLine("s1: {0}", s1.ToString());
+            Console.WriteLine("s2: {0}", s2.ToString());
+            Console.WriteLine("s3: {0}", s3.ToString());
+            Console.WriteLine("s1Copy: {0}", s1Copy.ToString());
+
+            Console.WriteLine("\n\n\n-----Tests-----");
+            Console.WriteLine("Equal Test 1 (false): {0}", Set.Equals(s1, s2));
+            Console.WriteLine("Equal Test 2 (true): {0}", Set.Equals(s1, s3));
+            Console.WriteLine("Contains Test 1 (false): {0}", s1.Contains(16));
+            Console.WriteLine("Contains Test 2 (true): {0}", s2.Contains(105));
+            Console.WriteLine("Union Test: {0}", s1s2Union.ToString());
+            Console.WriteLine("Intersection Test: {0}", s1s2Intersection.ToString());
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ week7/week7/Set.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week7
+{
+    public class Set : ISet
+    {
+        public int[] Items { get; private set; }
+
+        private Set() { }
+
+        public Set(params int[] ints)
+        {
+            this.Items = ints.Select(x => x).Distinct().ToArray();
+        }
+
+        public Set(IEnumerable<int> ints)
+        {
+            this.Items = ints.Select(x => x).Distinct().ToArray();
+        }
+
+        public bool Contains(int item)
+        {
+            return this.Items.Contains(item);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != typeof(Set))
+            {
+                return false;
+            }
+            
+            return this.Equals(obj as Set);
+        }
+
+        private bool Equals(Set s)
+        {
+            if (this.Items.Length != s.Items.Length)
+                return false;
+
+            for (int i = 0; i < this.Items.Length; ++i)
+            {
+                if (this.Contains(s.Items[i]))
+                    continue;
+
+                return false;
+            }
+
+            return true;
+        }
+
+        public static Set Union(Set s1, Set s2)
+        {
+            Set result = new Set(s1.Items.Concat(s2.Items));
+
+            return result;
+        }
+
+        public static Set Intersection(Set s1, Set s2)
+        {
+            Set result = new Set(s1.Items.Intersect(s2.Items));
+
+            return result;
+        }
+
+        public static bool Equals(Set s1, Set s2)
+        {
+            return s1.Equals(s2);
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            int count = 0;
+            foreach (var i in Items.OrderBy(x => x))
+            {
+                count++;
+                sb.Append(i);
+                if (count < Items.Count())
+                    sb.Append(",");
+            }
+            return sb.ToString();
+            
+        }
+    }
+}
